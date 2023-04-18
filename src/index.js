24b5e1404695250d7b3b3175564852f3a425db85
Grace <grace@lawn-128-61-115-33.lawn.gatech.edu>
2023-04-18 10:58:21 -0400

    initial dialogues, images

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import * as ReactDOMClient from 'react-dom/client';
+
 import './App.css';
+
 import * as d3 from "d3";
+
+import dialogues from "./dialogue.json";
 
 const App = () => {
 
-	const [backgroundUrl, setBackgroundUrl] = React.useState("url(/home.jpeg)");
+	const [backgroundUrl, setBackgroundUrl] = React.useState("url(home.jpeg)");
 	const [events, setEvents] = React.useState();
 	// const [eventCount, setEventCount] = React.useState();
-
-	let string = "testing";
 
 	function playAudio() {
 		var promise = document.querySelector("audio").play();
@@ -30,56 +32,91 @@ const App = () => {
 
 	function start() {
 
-		playAudio();
+		// playAudio();
 
-		setBackgroundUrl("url(/neighborhood.jpg)");
+		// setBackgroundUrl("url(neighborhood.jpg)");
 
 		d3.selectAll(".button").style("display", "none");
 
-		let start_events = [{"event":"dialogue", "text":"June 12th, 20XX"},
-							{"event":"dialogue", "text":"It's another warm day in Seoul."},
-							{"event":"dialogue", "text":"The neighborhood ladies are making their morning jog through the park again."},
-							{"event":"dialogue", "text":"And you are on your way home from the local convenience store, when you hear your phone buzzing..."},
-							{"event":"option", "text":["You pick up your phone"]}];
+		let start_events = dialogues["start"];
 
 		setEvents({"allEvents":start_events, "eventCount":0});
+		// play({"allEvents":start_events, "eventCount":0});
 
 	}
 
 	useEffect(() => {
 
+		d3.select("#options")
+		  .selectAll(".button")
+		  .style("visibility", "hidden")
+
 		if (events) {
+			
 			let e = events.allEvents[events.eventCount];
 
+			if (e.image) {
+				d3.select("#background").style("background-image", `url(${e.image})`);
+			}
+
+			if (e.show) {
+				d3.select("#yourDate")
+					.attr("src", `${e.show}.png`)
+					.style("visibility", `visible`);
+			} else {
+				d3.select("#yourDate").style("visibility", `hidden`);
+			}
+
+			if (e.speaker && e.speaker === "date") {
+				console.log(e.speaker)
+				d3.select("#textContainer").style("color", `pink`);
+			} else {
+				d3.select("#textContainer").style("color", `white`);
+			}
+
 			if (e.event === "dialogue") {
-				d3.select("#textbox")
+				d3.select("#textContainer")
 					.style("visibility", "visible")
 					.style("width", "100%")
-					.text(e.text)
+					.style("line-height", "2")
 				.on("click", () => {
 					let newEvents = {...events, "eventCount":events.eventCount+1};
 					setEvents(newEvents);
 				})
+
+				d3.select("#textbox").text(e.text)
 			} else if ((e.event === "option")) {
-				console.log("here");
-				d3.select(".button")
-					.attr("value", e.text[0])
+				d3.select("#options")
+					.selectAll(".button")
+					.data(e.text)
+					.join("input")
+					.attr("class", "button")
+					.attr("type", "button")
+					.attr("value", d => d[0])
+					.style("visibility", "visible")
 					.style("display", "block")
+					.style("margin-top", "20px")
+					.style("margin-bottom", "20px")
+					.on("click", function (e, d) {
+						let newEvents = {"allEvents":[...dialogues[d[1]]], "eventCount":0};
+						setEvents(newEvents);
+					})
 			}
 
 			
 		}
-
-		
 
 	}, [events])
 
 	let button_style = {
 		"width":"30%",
 		"padding:":"25px",
+		"display":"flex",
+		"justifyContent":"center",
+		"flexDirection":"column"
 	}
 
-	let text_style = {
+	let text_container = {
 		"visibility":"hidden",
 		"width": "0%",
 		"height": "30%",
@@ -87,9 +124,23 @@ const App = () => {
 	    "color": "white",
 	    "fontFamily": "sans-serif",
 	    "paddingTop": "20px",
-	    "paddingLeft": "50px",
+	    "paddingLeft": "100px",
 	    "position": "fixed",
-	    "bottom":0
+	    "bottom":0,
+	    "overflow":"hidden"
+	}
+
+	let text_style = {
+		"width": "80%"
+	}
+
+	let date_style = {
+		"height":"80%",
+		"backgroundSize":"cover",
+		"position":"fixed",
+		"right": 0,
+		"bottom": 0,
+		"visibility": "hidden"
 	}
 
 	return (
@@ -99,10 +150,18 @@ const App = () => {
 							"backgroundImage":backgroundUrl,
 							"backgroundSize":"cover",
 							"display":"flex",
+							"flexDirection":"column",
 							"alignItems":"center",
 							"justifyContent":"center"}}>
-			<p style={text_style} id="textbox"></p>
-			<input className="button" style={button_style} type="button" value="START GAME" onClick={start} />
+			<img id="yourDate" style={date_style} />
+			<div id="textContainer" style={text_container}>
+				<p style={text_style} id="textbox"></p>
+			</div>
+			<div id="buttons" style={button_style}>
+				<input className="button" type="button" value="START GAME" onClick={start} />
+			</div>
+			<div id="options" style={button_style}>
+			</div>
 			<audio loop>
 				<source src="/gangnam_audio.mp3" type="audio/mpeg" />
 			</audio>
